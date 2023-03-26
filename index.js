3917061916363a8d0396fd844955f2b11852e3ec
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-26 07:58:26 -0300

    [ADDED] variable reference for svg

--- index.js
+++ index.js
@@ -2,17 +2,32 @@
 let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 let req = new XMLHttpRequest();
 let values = [];
-//fetching JSON data
-req.open('GET', url, true);
-req.onload =()=> {
-    values = JSON.parse(req.responseText)
-}
-req.send()
+
+//refering svg from html
+let svg = d3.select('svg');
 
 //svg dimensions
 let width = 800;
 let height = 600;
 let padding = 40;
+
+//draws canvas with the dimensions set above
+let drawCanvas =()=> {
+    svg.attr('width', width)
+    svg.attr('height', height)
+};
+
+//reference to the position of values in x and y graph
+let xScale;
+let yScale;
+
+//fetching JSON data
+req.open('GET', url, true);
+req.onload =()=> {
+    values = JSON.parse(req.responseText)
+    drawCanvas();
+}
+req.send()
 
 
 
