567bbb5fe472893d3531d62ba0cc55b275347a7c
Joaquin Martinez Maneiro <joaquinmar08@gmail.com>
2023-03-26 07:27:53 -0300

    [ADDED] title to complete user story 1

---
+++ index.js
@@ -0,0 +1,11 @@
+//these are gonna be used to fetch JSON data from the dataset into the values variable
+let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+let req = new XMLHttpRequest();
+let values = [];
+//fetching JSON data
+req.open('GET', url, true);
+req.onload =()=> {
+    values = JSON.parse(req.responseText)
+}
+req.send()
+
